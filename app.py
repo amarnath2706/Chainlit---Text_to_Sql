82a3022f705f1617dc049e0c70a95accdc26bb19
Amar <amarnath.arjsd@gmail.com>
2024-06-05 14:15:26 +0530

    updated code

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from openai import AsyncOpenAI  
 from chainlit.playground.providers.openai import ChatOpenAI
-
+from dotenv import load_dotenv
 print("ok")
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-client=AsyncOpenAI(OPENAI_API_KEY)
+client=AsyncOpenAI(api_key=OPENAI_API_KEY)
 print("client created")
